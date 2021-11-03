091b9571db5a101b86a1762e865d2673363bb2cc
Kaan Çetin <kaan.cetin@yemeksepeti.com>
2021-11-03 15:19:30 +0300

    Remove unused extension

--- MoviesTests/MainPageTests/MoviesMainPageTests.swift
+++ MoviesTests/MainPageTests/MoviesMainPageTests.swift
@@ -41,20 +41,3 @@ class MoviesMainPageTests: XCTestCase {
         XCTAssertEqual(sut.nextPage, 3)
     }
 }
-
-extension TestScheduler {
-    /**
-     Creates a `TestableObserver` instance which immediately subscribes to the `source`
-     */
-    func record<O: ObservableConvertibleType>(
-        _ source: O,
-        disposeBag: DisposeBag
-    ) -> TestableObserver<O.Element> {
-        let observer = self.createObserver(O.Element.self)
-        source
-            .asObservable()
-            .bind(to: observer)
-            .disposed(by: disposeBag)
-        return observer
-    }
-}
